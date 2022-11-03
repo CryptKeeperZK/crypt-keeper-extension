ca26a3fa8fcb7c555774280da29fc949d62456d9
Iskander <iskander.andrews@protonmail.com>
2022-11-03 16:00:13 +0200

    fix: trying to fix `Uncaught ReferenceError: Worker is not defined`

--- src/background/backgroundPage.ts
+++ src/background/backgroundPage.ts
@@ -6,28 +6,40 @@ import RPCAction from "@src/util/constants";
 // TODO consider adding inTest env
 const app: ZkKepperController = new ZkKepperController();
 
-app.initialize().then(async () => {
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  browser.runtime.onMessage.addListener(async (request: Request, _) => {
+try {
+  // TODO: just added to check the difference in testing
+  chrome.runtime.onInstalled.addListener(async () => {
     try {
-      const res = await app.handle(request);
-      return [null, res];
-    } catch (e: any) {
-      return [e.message, null];
+      console.log("Extension is installed")
+    } catch (e) {
+      console.log("Extension is not installed");
     }
   });
+  app.initialize().then(async () => {
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    browser.runtime.onMessage.addListener(async (request: Request, _) => {
+      try {
+        const res = await app.handle(request);
+        return [null, res];
+      } catch (e: any) {
+        return [e.message, null];
+      }
+    });
 
-  // TODO: change to BrowserUtils 
-  browser.runtime.onInstalled.addListener(async ({ reason }) => {
-    console.log("Inside browser.runtime.connect().onInstalled");
-    if (reason === "install") {
-      // TODO open html where password will be interested
-      // browser.tabs.create({
-      //   url: 'popup.html'
-      // });
-    }
-    if (process.env.NODE_ENV === "development") {
-      // browser.tabs.create({ url: 'popup.html' });
-    }
+    // TODO: change to BrowserUtils
+    browser.runtime.onInstalled.addListener(async ({ reason }) => {
+      console.log("Inside browser.runtime.connect().onInstalled");
+      if (reason === "install") {
+        // TODO open html where password will be interested
+        // browser.tabs.create({
+        //   url: 'popup.html'
+        // });
+      }
+      if (process.env.NODE_ENV === "development") {
+        // browser.tabs.create({ url: 'popup.html' });
+      }
+    });
   });
-}); 
+} catch (error) {
+  console.log("Error in backgound!!1");
+}
--- src/background/controllers/browser-utils.ts
+++ src/background/controllers/browser-utils.ts
@@ -1,73 +1,74 @@
-import { browser } from 'webextension-polyfill-ts'
-import LockService from '../services/lock'
+import { browser } from "webextension-polyfill-ts";
+import LockService from "../services/lock";
 
 class BrowserUtils {
-    cached: any | null
+  cached: any | null;
 
-    constructor() {
-        this.removeWindow(async (windowId: any) => {
-            console.log("Inside removeWindow onRemove");
-            
-            try {
-                // TODO: Check either internalLogout() or logout()
-                //await LockService.internalLogout();
-                console.log("Inside removeWindow onRemove locked");
-                if (this.cached?.id === windowId) {
-                    this.cached = null
-                    console.log("Inside removeWindow onRemove cleaned");
-                }
-            } catch (error) {
-                console.log("Inside removeWindow onRemove error", error);
-            }
-        });
-    }
+  constructor() {
+    this.removeWindow((windowId: any) => {
+      console.log("Inside removeWindow onRemove");
 
-    createTab = async (options: any) => browser.tabs.create(options)
-
-    removeWindow = (callback: any) => {
-        browser.windows.onRemoved.addListener(callback);
-    }
-
-    createWindow = async (options: any) => browser.windows.create(options)
-
-    openPopup = async () => {
-        if (this.cached) {
-            this.focusWindow(this.cached.id)
-            return this.cached
+      try {
+        // TODO: Check either internalLogout() or logout()
+        //await LockService.internalLogout();
+        console.log("Inside removeWindow onRemove locked");
+        if (this.cached?.id === windowId) {
+          this.cached = null;
+          console.log("Inside removeWindow onRemove cleaned");
         }
+      } catch (error) {
+        console.log("Inside removeWindow onRemove error", error);
+      }
+    });
+  }
 
-        const tab = await this.createTab({ url: 'popup.html', active: false })
+  createTab = async (options: any) => browser.tabs.create(options);
 
-        // TODO add this in config/constants...
-        const popup = await this.createWindow({
-            tabId: tab.id,
-            type: 'popup',
-            focused: true,
-            width: 357,
-            height: 600
-        })
+  removeWindow = (callback: any) => {
+    // TODO: Converted from browser. to chrome. solved the error
+    chrome.windows.onRemoved.addListener(callback);
+  };
 
-        this.cached = popup
-        return popup
+  createWindow = async (options: any) => browser.windows.create(options);
+
+  openPopup = async () => {
+    if (this.cached) {
+      this.focusWindow(this.cached.id);
+      return this.cached;
     }
 
-    closePopup = async () => {
-        if (this.cached) {
-            browser.windows.remove(this.cached.id)
-        }
+    const tab = await this.createTab({ url: "popup.html", active: false });
+
+    // TODO add this in config/constants...
+    const popup = await this.createWindow({
+      tabId: tab.id,
+      type: "popup",
+      focused: true,
+      width: 357,
+      height: 600,
+    });
+
+    this.cached = popup;
+    return popup;
+  };
+
+  closePopup = async () => {
+    if (this.cached) {
+      browser.windows.remove(this.cached.id);
     }
+  };
 
-    focusWindow = (windowId) => browser.windows.update(windowId, { focused: true })
+  focusWindow = windowId => browser.windows.update(windowId, { focused: true });
 
-    getAllWindows = () => browser.windows.getAll()
+  getAllWindows = () => browser.windows.getAll();
 
-    activatedTabs = (callback: any) => {
-        browser.tabs.onActivated.addListener(callback);
-    }
+  activatedTabs = (callback: any) => {
+    browser.tabs.onActivated.addListener(callback);
+  };
 
-    sendMessageTabs = async (tabId: number, message: any) => {
-        await browser.tabs.sendMessage(tabId, message);
-    }
+  sendMessageTabs = async (tabId: number, message: any) => {
+    await browser.tabs.sendMessage(tabId, message);
+  };
 }
 
-export default new BrowserUtils()
+export default new BrowserUtils();
