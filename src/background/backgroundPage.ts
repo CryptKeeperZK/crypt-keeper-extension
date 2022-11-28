f4e4601dffba26a0df09cee28a9222ba07b3a325
Iskander <iskander.andrews@protonmail.com>
2022-11-28 16:35:55 +0200

    fix: trying to resolve `Could not establish connection. Receiving end does not exist.`

--- src/background/backgroundPage.ts
+++ src/background/backgroundPage.ts
@@ -1,47 +1,73 @@
 // TODO: importing scripts better using importScritps() check MM 
 import 'subworkers'; 
-import { browser } from "webextension-polyfill-ts";
+import { browser, Runtime } from "webextension-polyfill-ts";
 import { Request } from "@src/types";
 import ZkKeeperController from "./zk-keeper";
 import RPCAction from "@src/util/constants";
+import { isManifestV3 } from './shared/checkManifestV3';
+import log from 'loglevel';
+
+log.setDefaultLevel(process.env.CRYPTKEEPER_DEBUG ? 'debug' : 'info');
 
 // TODO consider adding inTest env
 const app: ZkKeeperController = new ZkKeeperController();
 
-try {
-  // TODO: just added to check the difference in testing
-  chrome.runtime.onInstalled.addListener(async () => {
-    try {
-      console.log("Extension is installed")
-    } catch (e) {
-      console.log("Extension is not installed");
-    }
-  });
-  app.initialize().then(async () => {
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    browser.runtime.onMessage.addListener(async (request: Request, _) => {
-      try {
-        const res = await app.handle(request);
-        return [null, res];
-      } catch (e: any) {
-        return [e.message, null];
-      }
-    });
+const initApp = async (remotePort?: chrome.runtime.Port) => {
+  chrome.runtime.onConnect.removeListener(initApp);
+  await initialize(remotePort);
+  log.info('CryptKeeper initialization complete.');
+}
 
-    // TODO: change to BrowserUtils
-    browser.runtime.onInstalled.addListener(async ({ reason }) => {
-      console.log("Inside browser.runtime.connect().onInstalled");
-      if (reason === "install") {
-        // TODO open html where password will be interested
-        // browser.tabs.create({
-        //   url: 'popup.html'
+if (isManifestV3) {
+  chrome.runtime.onConnect.addListener(initApp);
+} else {
+  initialize().catch(log.error);   
+}
+
+async function initialize (remotePort?: chrome.runtime.Port) {
+  if (remotePort) {
+    try {
+      // TODO: just added to check the difference in testing
+      // chrome.runtime.onInstalled.addListener(async () => {
+      //   try {
+      //     console.log("Extension is installed")
+      //   } catch (e) {
+      //     console.log("Extension is not installed");
+      //   }
+      // });
+      app.initialize().then(async () => {
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars
+        remotePort.onMessage.addListener(async (request: Request, _) => {
+          try {
+            console.log("Background 1: request: ", request)
+            const res = await app.handle(request);
+            return [null, res];
+          } catch (e: any) {
+            console.log("Background 2: request: ", request)
+            console.log("ERROR!!!!!!!!!")
+            return [e.message, null];
+          }
+        });
+        //browser.runtime.onMessage.addListener();
+    
+        // TODO: change to BrowserUtils
+        // browser.runtime.onInstalled.addListener(async ({ reason }) => {
+        //   console.log("Inside browser.runtime.connect().onInstalled");
+        //   if (reason === "install") {
+        //     // TODO open html where password will be interested
+        //     // browser.tabs.create({
+        //     //   url: 'popup.html'
+        //     // });
+        //   }
+        //   if (process.env.NODE_ENV === "development") {
+        //     // browser.tabs.create({ url: 'popup.html' });
+        //   }
         // });
-      }
-      if (process.env.NODE_ENV === "development") {
-        // browser.tabs.create({ url: 'popup.html' });
-      }
-    });
-  });
-} catch (error) {
-  console.log("Error in backgound!!1");
+      });
+    } catch (error) {
+      console.log("Error in backgound!!1");
+    }
+  } else {
+    log.error("remotePort is not initalized");
+  }
 }
--- src/ui/popup.tsx
+++ src/ui/popup.tsx
@@ -1,26 +1,36 @@
 import * as React from 'react'
-import * as ReactDOM from 'react-dom'
+//import { createRoot } from 'react-dom/client';
+import ReactDOM from "react-dom/client";
 import { browser } from 'webextension-polyfill-ts'
 import Popup from '@src/ui/pages/Popup'
 import { Provider } from 'react-redux'
 import { store } from '@src/ui/store/configureAppStore'
 import { HashRouter } from 'react-router-dom'
 
+chrome.tabs.query({ active: true, currentWindow: true }).then(() => {
+    const port = chrome.runtime.connect({name: "New connection"});
 
-browser.runtime.onMessage.addListener((action) => {
-    if (action?.type) {
-        store.dispatch(action)
-    }
-})
-
-browser.tabs.query({ active: true, currentWindow: true }).then(() => {
-    browser.runtime.connect()
-    ReactDOM.render(
+    port.onMessage.addListener((action) => {
+        if (action?.type) {
+            store.dispatch(action)
+        }
+    });
+    
+    const root = ReactDOM.createRoot(document.getElementById("popup") as HTMLElement);
+    root.render(
         <Provider store={store}>
             <HashRouter>
                 <Popup />
             </HashRouter>
-        </Provider>,
-        document.getElementById('popup')
-    )
+        </Provider>
+    );
+
+    // ReactDOM.render(
+    //     <Provider store={store}>
+    //         <HashRouter>
+    //             <Popup />
+    //         </HashRouter>
+    //     </Provider>,
+    //     document.getElementById('popup')
+    // )
 })
