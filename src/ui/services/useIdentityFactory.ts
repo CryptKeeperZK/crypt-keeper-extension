aba8f7a02766065de2bd7602d69df99fbb8e0edc
0xmad <>
2023-02-08 10:32:32 -0600

    chore: code review fixes

--- src/ui/services/useIdentityFactory.ts
+++ src/ui/services/useIdentityFactory.ts
@@ -1,9 +1,11 @@
-import { CreateIdentityWeb2Provider } from "@src/types";
-import { useMetaMaskWalletInfo } from "./useMetaMask";
+import { CreateIdentityWeb2Provider, WalletInfoBackgound } from "@src/types";
 
-export async function useIdentityFactory(web2Provider: CreateIdentityWeb2Provider, nonce: number): Promise<string | undefined> {
-    const walletInfo = await useMetaMaskWalletInfo();
-    const message = `Sign this message to generate your ${web2Provider} Semaphore identity with key nonce: ${nonce}.`;
-    
-    return walletInfo?.signer.signMessage(message);
+export async function useIdentityFactory(
+  web2Provider: CreateIdentityWeb2Provider,
+  nonce: number,
+  walletInfo: WalletInfoBackgound | null,
+): Promise<string | undefined> {
+  const message = `Sign this message to generate your ${web2Provider} Semaphore identity with key nonce: ${nonce}.`;
+
+  return walletInfo?.signer.signMessage(message);
 }
