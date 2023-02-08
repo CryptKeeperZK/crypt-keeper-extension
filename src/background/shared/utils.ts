db7f307ec98006883e9429d87ef57f7e92556610
Iskander <iskander.andrews@protonmail.com>
2023-02-08 21:08:24 +0200

    chore: push utils

---
+++ src/background/shared/utils.ts
@@ -0,0 +1,27 @@
+/**
+ * @src Metamask extension
+ * A deferred Promise.
+ *
+ * A deferred Promise is one that can be resolved or rejected independently of
+ * the Promise construction.
+ *
+ * @typedef {object} DeferredPromise
+ * @property {Promise} promise - The Promise that has been deferred.
+ * @property {() => void} resolve - A function that resolves the Promise.
+ * @property {() => void} reject - A function that rejects the Promise.
+ */
+
+/**
+ * Create a defered Promise.
+ *
+ * @returns {DeferredPromise} A deferred Promise.
+ */
+export function deferredPromise() {
+  let resolve;
+  let reject;
+  const promise = new Promise((innerResolve, innerReject) => {
+    resolve = innerResolve;
+    reject = innerReject;
+  });
+  return { promise, resolve, reject };
+}
