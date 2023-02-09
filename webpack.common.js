ae1ce6b6d5a8e13c28c9021180f5041db42d3a1f
0xmad <>
2023-02-09 10:57:44 -0600

    fix: build extension
    - [x] Remove dist folder
    - [x] Move manifest.json and popup.html to src folder
    - [x] Add html webpack plugin for popup.html
    - [x] Add copy webpack plugin to copy assets (logos, manifest)

--- webpack.common.js
+++ webpack.common.js
@@ -1,100 +1,115 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable import/no-extraneous-dependencies */
-const webpack = require('webpack')
-const path = require('path')
-const CopyPlugin = require('copy-webpack-plugin')
+const webpack = require("webpack");
+const path = require("path");
+const CopyPlugin = require("copy-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
-const envPlugin = new webpack.EnvironmentPlugin(['NODE_ENV'])
+const envPlugin = new webpack.EnvironmentPlugin(["NODE_ENV"]);
 
 module.exports = {
-    entry: {
-        injected: path.join(__dirname, 'src/contentscripts/injected.ts'),
-        content: path.join(__dirname, 'src/contentscripts/index.ts'),
-        backgroundPage: path.join(__dirname, 'src/background/backgroundPage.ts'),
-        popup: path.join(__dirname, 'src/ui/popup.tsx')
-    },
-    output: {
-        path: path.join(__dirname, 'dist/js'),
-        filename: '[name].js'
-    },
-    plugins: [
-        envPlugin,
-        new webpack.ProvidePlugin({
-            Buffer: ['buffer', 'Buffer']
-        }),
-        new webpack.ProvidePlugin({
-            process: 'process/browser'
-        })
-    ],
-    module: {
-        rules: [
-            {
-                exclude: /node_modules/,
-                test: /\.tsx?$/,
-                use: 'ts-loader'
-            },
-            // TODO: string-replace-loader has issues with new webpack version
-            // {
-            //     test: /ejs\.js$/,
-            //     loader: 'string-replace-loader',
-            //     options: {
-            //         search: "(new Function('return this;'))().Promise",
-            //         replace: "(function() { return this ? this : typeof self !== 'undefined' ? self : undefined})() || Function('return this')()",
-            //         flags: 'g',
-            //         strict: true
-            //     }
-            // },
-            {
-                exclude: /node_modules/,
-                test: /\.scss$/,
-                use: [
-                    {
-                        loader: 'style-loader' // Creates style nodes from JS strings
-                    },
-                    {
-                        loader: 'css-loader' // Translates CSS into CommonJS
-                    },
-                    {
-                        loader: 'sass-loader' // Compiles Sass to CSS
-                    }
-                ]
-            },
-            {
-                test: /\.(gif|png|jpe?g|svg)$/i,
-                use: [
-                    'file-loader',
-                    {
-                        loader: 'image-webpack-loader',
-                        options: {
-                            publicPath: 'assets',
-                            bypassOnDebug: true, // webpack@1.x
-                            disable: true // webpack@2.x and newer
-                        }
-                    }
-                ]
-            }
-        ]
-    },
-    resolve: {
-        extensions: ['.ts', '.tsx', '.js', '.png', '.svg'],
-        alias: {
-            '@src': path.resolve(__dirname, 'src/'),
-            buffer: 'buffer'
+  entry: {
+    injected: path.join(__dirname, "src/contentscripts/injected.ts"),
+    content: path.join(__dirname, "src/contentscripts/index.ts"),
+    backgroundPage: path.join(__dirname, "src/background/backgroundPage.ts"),
+    popup: path.join(__dirname, "src/ui/popup.tsx"),
+  },
+  output: {
+    path: path.join(__dirname, "dist/js"),
+    filename: "[name].js",
+  },
+  plugins: [
+    envPlugin,
+    new webpack.ProvidePlugin({
+      Buffer: ["buffer", "Buffer"],
+    }),
+    new webpack.ProvidePlugin({
+      process: "process/browser",
+    }),
+    new CopyPlugin({
+      patterns: [
+        { from: path.join(__dirname, "./src/static/icons/*"), to: path.join(__dirname, "./dist/[name][ext]") },
+        {
+          from: path.join(__dirname, "./src/manifest.json"),
+          to: path.join(__dirname, "./dist/manifest.json"),
         },
-        fallback: {
-            browserify: require.resolve('browserify'),
-            stream: require.resolve('stream-browserify'),
-            path: require.resolve('path-browserify'),
-            crypto: require.resolve('crypto-browserify'),
-            os: require.resolve('os-browserify/browser'),
-            http: require.resolve('stream-http'),
-            https: require.resolve('https-browserify'),
-            fs: false,
-            assert: false,
-            zlib: false,
-            constants: false
-        }
+      ],
+    }),
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, "./src/popup.html"),
+      filename: "../popup.html",
+      chunks: ["popup"],
+      minify: true,
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        exclude: /node_modules/,
+        test: /\.tsx?$/,
+        use: "ts-loader",
+      },
+      // TODO: string-replace-loader has issues with new webpack version
+      // {
+      //     test: /ejs\.js$/,
+      //     loader: 'string-replace-loader',
+      //     options: {
+      //         search: "(new Function('return this;'))().Promise",
+      //         replace: "(function() { return this ? this : typeof self !== 'undefined' ? self : undefined})() || Function('return this')()",
+      //         flags: 'g',
+      //         strict: true
+      //     }
+      // },
+      {
+        exclude: /node_modules/,
+        test: /\.scss$/,
+        use: [
+          {
+            loader: "style-loader", // Creates style nodes from JS strings
+          },
+          {
+            loader: "css-loader", // Translates CSS into CommonJS
+          },
+          {
+            loader: "sass-loader", // Compiles Sass to CSS
+          },
+        ],
+      },
+      {
+        test: /\.(gif|png|jpe?g|svg)$/i,
+        use: [
+          "file-loader",
+          {
+            loader: "image-webpack-loader",
+            options: {
+              publicPath: "assets",
+              bypassOnDebug: true, // webpack@1.x
+              disable: true, // webpack@2.x and newer
+            },
+          },
+        ],
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".ts", ".tsx", ".js", ".png", ".svg"],
+    alias: {
+      "@src": path.resolve(__dirname, "src/"),
+      buffer: "buffer",
     },
-    externals: /^(worker_threads)$/
-}
-
+    fallback: {
+      browserify: require.resolve("browserify"),
+      stream: require.resolve("stream-browserify"),
+      path: require.resolve("path-browserify"),
+      crypto: require.resolve("crypto-browserify"),
+      os: require.resolve("os-browserify/browser"),
+      http: require.resolve("stream-http"),
+      https: require.resolve("https-browserify"),
+      fs: false,
+      assert: false,
+      zlib: false,
+      constants: false,
+    },
+  },
+  externals: /^(worker_threads)$/,
+};
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,13 +1,13 @@
 /* eslint-disable import/extensions */
 /* eslint-disable import/no-extraneous-dependencies */
-const { merge } = require('webpack-merge')
-const common = require('./webpack.common.js')
+const { merge } = require("webpack-merge");
+const common = require("./webpack.common.js");
 
 module.exports = merge(common, {
-    mode: 'production',
-    performance: {
-        hints: false,
-        maxEntrypointSize: 512000,
-        maxAssetSize: 512000
-    }    
-})
+  mode: "production",
+  performance: {
+    hints: false,
+    maxEntrypointSize: 512000,
+    maxAssetSize: 512000,
+  },
+});
