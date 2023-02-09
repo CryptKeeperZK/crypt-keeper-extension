7d3e1ee29eaa0045fdda49ae5a2ebf79399b58a5
0xmad <unknown>
2023-02-09 10:57:44 -0600

    fix: build extension
    - [x] Remove dist folder
    - [x] Move manifest.json and popup.html to src folder
    - [x] Add html webpack plugin for popup.html
    - [x] Add copy webpack plugin to copy assets (logos, manifest)

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,9 +1,9 @@
 /* eslint-disable import/no-extraneous-dependencies */
 /* eslint-disable import/extensions */
-const { merge } = require('webpack-merge');
-const common = require('./webpack.common.js')
+const { merge } = require("webpack-merge");
+const common = require("./webpack.common.js");
 
 module.exports = merge(common, {
-    mode: 'development',
-    devtool: 'inline-source-map'
-})
+  mode: "development",
+  devtool: "inline-source-map",
+});
