9351b81f4dd7ab625bb12bce3a934b7787ad5327
0xmad <unknown>
2023-02-14 10:04:10 -0600

    chore: code style fixes

--- src/util/checkParameter.ts
+++ src/util/checkParameter.ts
@@ -1,13 +1,13 @@
 export default function checkParameter(
-    value: any,
-    name: string,
-    type: 'boolean' | 'number' | 'string' | 'object' | 'function'
+  value: any,
+  name: string,
+  type: "boolean" | "number" | "string" | "object" | "function",
 ) {
-    if (value === undefined) {
-        throw new TypeError(`Parameter '${name}' is not defined`)
-    }
+  if (value === undefined) {
+    throw new TypeError(`Parameter '${name}' is not defined`);
+  }
 
-    if (typeof value !== type) {
-        throw new TypeError(`Parameter '${name}' is not ${type === 'object' ? 'an' : 'a'} ${type}`)
-    }
+  if (typeof value !== type) {
+    throw new TypeError(`Parameter '${name}' is not ${type === "object" ? "an" : "a"} ${type}`);
+  }
 }
--- src/util/pushMessage.ts
+++ src/util/pushMessage.ts
@@ -1,10 +1,10 @@
-import { ReduxAction } from '@src/types'
-import { browser } from 'webextension-polyfill-ts'
+import { ReduxAction } from "@src/types";
+import { browser } from "webextension-polyfill-ts";
 
 export default async function pushMessage(message: ReduxAction) {
-    // if (chrome && chrome.runtime) {
-    //     return chrome.runtime.sendMessage(message)
-    // }
+  // if (chrome && chrome.runtime) {
+  //     return chrome.runtime.sendMessage(message)
+  // }
 
-    return browser.runtime.sendMessage(message)
+  return browser.runtime.sendMessage(message);
 }
