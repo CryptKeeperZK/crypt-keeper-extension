b8cc06ca5985133fb9c9ea9ab26c8b64e4986c90
0xmad <>
2023-02-15 10:46:29 -0600

    fix: move atob/btoa to globals

---
+++ scripts/globals.ts
@@ -0,0 +1,7 @@
+if (!global.atob) {
+  global.atob = (str: string) => Buffer.from(str, "base64").toString("binary");
+}
+
+if (!global.btoa) {
+  global.btoa = (str: string) => Buffer.from(str, "binary").toString("base64");
+}
