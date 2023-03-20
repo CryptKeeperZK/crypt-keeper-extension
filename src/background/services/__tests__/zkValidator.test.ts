b85d6dcffd0c1a76f1f6a40e3c72f800b7c9ba46
0xmad <0xmad@users.noreply.github.com>
2023-03-20 16:52:55 -0500

    fix: zk input validation logic

--- src/background/services/__tests__/zkValidator.test.ts
+++ src/background/services/__tests__/zkValidator.test.ts
@@ -25,6 +25,16 @@ describe("background/services/zkValidator", () => {
     expect(result).toStrictEqual(args);
   });
 
+  test("should validate proof properly with storage address", () => {
+    const args = {
+      merkleStorageAddress: "merkleStorageAddress",
+    } as Required<ZkInputs>;
+
+    const result = validateZkInputs(args);
+
+    expect(result).toStrictEqual(args);
+  });
+
   test("should throw error if there is no any input", () => {
     expect(() => validateZkInputs({} as Required<ZkInputs>)).toThrowError("no proof provided");
   });
--- src/background/services/zkValidator.ts
+++ src/background/services/zkValidator.ts
@@ -3,12 +3,14 @@ import { ZkInputs } from "@src/types";
 import { MerkleProofValidator, ArtifactsProofValidator } from "./validation";
 
 export const validateZkInputs = (payload: Required<ZkInputs>): Required<ZkInputs> => {
-  const { merkleProofArtifacts, merkleProof } = payload;
+  const { merkleProofArtifacts, merkleProof, merkleStorageAddress } = payload;
 
   if (merkleProof) {
     new MerkleProofValidator(merkleProof).validateProof();
   } else if (merkleProofArtifacts) {
     new ArtifactsProofValidator(merkleProofArtifacts).validateProof();
+  } else if (merkleStorageAddress) {
+    return payload;
   } else {
     throw new Error("no proof provided");
   }
