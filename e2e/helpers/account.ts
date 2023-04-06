7886fa9f0bb4ec2eb91774b53d9d116830d58f3e
Anton <14254374+0xmad@users.noreply.github.com>
2023-04-06 09:35:51 -0500

    test: setup playwright (#244)
    - [x] Install playwright boilerplate
    - [x] Add basic smoke test for initialization and login
    - [x] Configure linter, prettier, jest and typescript to work with playwright
    - [x] Add github action with master push, manual triggers and scheduled check
    
    Co-authored-by: 0xmad <0xmad@users.noreply.github.com>

---
+++ e2e/helpers/account.ts
@@ -0,0 +1,39 @@
+import type { Page } from "@playwright/test";
+
+import { CRYPT_KEEPER_PASSWORD } from "../constants";
+import { type TestExtension, expect } from "../fixtures";
+
+export async function createAccount({ app, cryptKeeper }: TestExtension): Promise<void> {
+  await connectCryptKeeper(app);
+
+  await cryptKeeper.getByLabel("Password", { exact: true }).type(CRYPT_KEEPER_PASSWORD);
+  await cryptKeeper.getByLabel("Confirm Password", { exact: true }).type(CRYPT_KEEPER_PASSWORD);
+  await cryptKeeper.getByText("Continue", { exact: true }).click();
+
+  await cryptKeeper.getByText("Approve").click();
+}
+
+export async function lockAccount({ app, cryptKeeperExtensionId }: TestExtension): Promise<void> {
+  await connectCryptKeeper(app);
+
+  await app.goto(`chrome-extension://${cryptKeeperExtensionId}/popup.html`);
+
+  await app.getByTestId("menu").click();
+  await app.getByText("Lock", { exact: true }).click();
+}
+
+export async function unlockAccount({ app, cryptKeeperExtensionId }: TestExtension): Promise<void> {
+  await app.goto(`chrome-extension://${cryptKeeperExtensionId}/popup.html`);
+
+  await app.getByLabel("Password", { exact: true }).type(CRYPT_KEEPER_PASSWORD);
+  await app.getByText("Unlock", { exact: true }).click();
+}
+
+async function connectCryptKeeper(app: Page): Promise<void> {
+  await app.reload();
+  await app.goto("/");
+
+  await expect(app).toHaveTitle(/Crypt-keeper Extension demo/);
+  await expect(app.getByText(/Please connect to Crypt-Keeper to continue./)).toBeVisible();
+  await app.getByText("Connect", { exact: true }).click();
+}
