15a1e940d40d5ae57f2f0f2b019797a906f103dc
iskdrews <36774944+iskdrews@users.noreply.github.com>
2023-04-07 14:37:01 +0200

    fix: reqested changes

--- src/ui/components/PasswordInput/usePasswordInput.ts
+++ src/ui/components/PasswordInput/usePasswordInput.ts
@@ -1,19 +1,19 @@
-import { useState } from "react";
+import { useCallback, useState } from "react";
 
 export interface IUsePasswordInput {
   isShowPassword: boolean;
-  setShowPassword: () => void;
+  onShowPassword: () => void;
 }
 
 export const usePasswordInput = (): IUsePasswordInput => {
   const [isShowPassword, setIsShowPassword] = useState(false);
 
-  const setShowPassword = () => {
+  const onShowPassword = useCallback(() => {
     setIsShowPassword((isShow) => !isShow);
-  };
+  }, [setIsShowPassword]);
 
   return {
     isShowPassword,
-    setShowPassword,
+    onShowPassword,
   };
 };
