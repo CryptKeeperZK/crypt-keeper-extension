368b4024f7964b022d032f905af74939d7364ea0
iskdrews <36774944+iskdrews@users.noreply.github.com>
2023-04-24 09:13:56 +0200

    chore: run prettier, and linter

--- src/background/backgroundPage.ts
+++ src/background/backgroundPage.ts
@@ -2,13 +2,13 @@ import log from "loglevel";
 import "subworkers";
 import { browser } from "webextension-polyfill-ts";
 
+import CryptKeeperController from "@src/background/cryptKeeper";
 import { deferredPromise } from "@src/background/shared/utils";
 import { isDebugMode } from "@src/config/env";
 import { RequestHandler } from "@src/types";
 
 import "./appInit";
 import "./shared/initGlobals";
-import CryptKeeperController from "./cryptKeeper";
 
 log.setDefaultLevel(isDebugMode() ? "debug" : "info");
 
