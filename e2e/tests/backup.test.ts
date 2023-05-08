cefd1e0efedde774730f6d9082afb5a597ed3c31
Anton <14254374+0xmad@users.noreply.github.com>
2023-05-08 10:14:43 +0300

    test: add e2e tests for backup download (#337)
    Co-authored-by: 0xmad <0xmad@users.noreply.github.com>

---
+++ e2e/tests/backup.test.ts
@@ -0,0 +1,34 @@
+import { expect, test } from "../fixtures";
+import { connectWallet, createAccount } from "../helpers/account";
+import { CryptKeeper } from "../pages";
+
+test.describe("backup", () => {
+  test.beforeEach(async ({ page, cryptKeeperExtensionId, context }) => {
+    await createAccount({ page, cryptKeeperExtensionId, context });
+
+    await page.goto(`chrome-extension://${cryptKeeperExtensionId}/popup.html`);
+    await expect(page.getByTestId("home-page")).toBeVisible();
+
+    await connectWallet({ page, cryptKeeperExtensionId, context });
+    await expect(page.getByText("Ethereum mainnet")).toBeVisible();
+  });
+
+  test("should download backup properly", async ({ page }) => {
+    const extension = new CryptKeeper(page);
+    await extension.focus();
+
+    await extension.identitiesTab.createIdentity();
+    await expect(extension.getByText("Account # 0")).toBeVisible();
+
+    await extension.settingsPage.openPage();
+    await extension.settingsPage.openTab("Advanced");
+    const path = await extension.settingsPage.downloadBackup();
+
+    expect(path).toBeDefined();
+
+    await extension.goHome();
+
+    await extension.activityTab.openTab();
+    await expect(extension.activityTab.getByText("Backup download")).toBeVisible();
+  });
+});
