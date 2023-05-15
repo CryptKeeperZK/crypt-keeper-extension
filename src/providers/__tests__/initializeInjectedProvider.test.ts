d2d860ab833e7e95547272d50d0454331dca5252
iskdrews <36774944+iskdrews@users.noreply.github.com>
2023-05-15 18:09:36 +0200

    test: unit test for initializeInjectedProvider

---
+++ src/providers/__tests__/initializeInjectedProvider.test.ts
@@ -0,0 +1,41 @@
+/**
+ * @jest-environment jsdom
+ */
+import { initializeInjectedProvider, setGlobalInjectedPrivider } from "../initializeInjectedProvider";
+import { CryptKeeperInjectedProvider } from "../sdk";
+
+jest.mock("@src/background/services/event", (): unknown => ({
+  __esModule: true,
+  default: jest.fn().mockImplementation(),
+}));
+
+jest.mock("@src/background/services/zkProof", (): unknown => ({
+  __esModule: true,
+  default: {
+    getInstance: jest.fn(),
+  },
+}));
+
+describe("InitialzieInjectedProvider", () => {
+  test("Should be window.cryptkeeper undefined yet", () => {
+    expect(window.cryptkeeper).not.toBeDefined();
+  });
+
+  test("Should be able to initialize CK injected provider", () => {
+    initializeInjectedProvider();
+    expect(window.cryptkeeper).toBeDefined();
+    expect(window.cryptkeeper.isCryptKeeper).toBe(true);
+    const dispatchEventSpy = jest.spyOn(window, "addEventListener");
+    expect(dispatchEventSpy).toHaveBeenCalledWith("message", window.cryptkeeper.eventResponser);
+  });
+
+  test("Should be able to set global injected provider", () => {
+    const injectedProvider = new CryptKeeperInjectedProvider();
+    setGlobalInjectedPrivider(injectedProvider);
+    expect(window.cryptkeeper).toBeDefined();
+    expect(window.cryptkeeper).toBe(injectedProvider);
+    expect(window.cryptkeeper.isCryptKeeper).toBe(true);
+    const dispatchEventSpy = jest.spyOn(window, "dispatchEvent");
+    expect(dispatchEventSpy).toHaveBeenCalledWith(new Event(`cryptkeeper#initialized`));
+  });
+});
