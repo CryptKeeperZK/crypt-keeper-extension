dcaa7ed258552e4df822a5c09b8384f1b54ae66f
iskdrews <36774944+iskdrews@users.noreply.github.com>
2023-05-16 16:26:36 +0200

    fix: run prettier and linter

--- src/background/services/event/__test__/EventEmitter.test.ts
+++ src/background/services/event/__test__/EventEmitter.test.ts
@@ -1,50 +1,54 @@
+import EventEmitter from "..";
 import { Events, EventHandler, EventName } from "../types";
-import EventEmitter from "../";
 
-let events: Events = {
-    "identityChanged": () => false,
-    "login": () => false,
-    "logout": () => false
-}
+const events: Events = {
+  identityChanged: () => false,
+  login: () => false,
+  logout: () => false,
+};
 
 jest.mock("nanoevents", (): unknown => ({
-    __esModule: true,
-    createNanoEvents: jest.fn(() => ({
-        on: jest.fn().mockImplementation((eventName: EventName, cb: EventHandler) => {
-            events[eventName] = cb;
-        }),
-        emit: jest.fn().mockImplementation((eventName: EventName) => {
-            const cb = events[eventName];
-            cb();
-        })
-    }))
-}))
+  __esModule: true,
+  createNanoEvents: jest.fn(() => ({
+    on: jest.fn().mockImplementation((eventName: EventName, cb: EventHandler) => {
+      events[eventName] = cb;
+    }),
+    emit: jest.fn().mockImplementation((eventName: EventName) => {
+      const cb = events[eventName];
+      cb();
+    }),
+  })),
+}));
 
 describe("background/services/event", () => {
-    test("Should be able to add new event listeners", () => {
-        let eventCondition: boolean = false
-        const eventHandler = (): boolean => eventCondition = !eventCondition;
-        const eventEmitter = new EventEmitter();
+  test("Should be able to add new event listeners", () => {
+    let eventCondition = false;
+    const eventHandler = (): void => {
+      eventCondition = !eventCondition;
+    };
+    const eventEmitter = new EventEmitter();
 
-        eventEmitter.on("identityChanged", eventHandler);
-        eventEmitter.emit("identityChanged");
-        // TOOD: check if the evnetHandler function is called or not.
-        //const cbSpy = jest.spyOn(events, "identityChanged");
-        //expect(cbSpy).toHaveBeenCalledWith(eventCondition); => Gives error still
-        expect(eventCondition).toBe(true);
-    });
+    eventEmitter.on("identityChanged", eventHandler);
+    eventEmitter.emit("identityChanged");
+    // TOOD: check if the evnetHandler function is called or not.
+    // const cbSpy = jest.spyOn(events, "identityChanged");
+    // expect(cbSpy).toHaveBeenCalledWith(eventCondition); => Gives error still
+    expect(eventCondition).toBe(true);
+  });
 
-    test("Should be able to clear events listeners", () => {
-        let eventCondition: boolean = false
-        const eventHandler = (): boolean => eventCondition = !eventCondition;
-        const eventEmitter = new EventEmitter();
+  test("Should be able to clear events listeners", () => {
+    let eventCondition = false;
+    const eventHandler = (): void => {
+      eventCondition = !eventCondition;
+    };
+    const eventEmitter = new EventEmitter();
 
-        eventEmitter.on("identityChanged", eventHandler);
-        eventEmitter.emit("identityChanged");
-        expect(eventCondition).toBe(true);
-        eventEmitter.emit("identityChanged");
-        expect(eventCondition).toBe(false);
-        eventEmitter.cleanListeners();
-        expect(eventCondition).toBe(false);
-    });
+    eventEmitter.on("identityChanged", eventHandler);
+    eventEmitter.emit("identityChanged");
+    expect(eventCondition).toBe(true);
+    eventEmitter.emit("identityChanged");
+    expect(eventCondition).toBe(false);
+    eventEmitter.cleanListeners();
+    expect(eventCondition).toBe(false);
+  });
 });
--- src/providers/sdk/__tests__/Base.test.ts
+++ src/providers/sdk/__tests__/Base.test.ts
@@ -18,24 +18,24 @@ jest.mock("@src/background/services/zkProof", (): unknown => ({
 jest.useRealTimers();
 
 const createTimeout = (): Promise<void> =>
-    new Promise((resolve) => {
-      // need to wait until we get popup and add request to queue
-      setTimeout(resolve, 10000);
-    });
+  new Promise((resolve) => {
+    // need to wait until we get popup and add request to queue
+    setTimeout(resolve, 10000);
+  });
 
 describe("providers/sdk/Base", () => {
-    test("Should be able to check if it is CK provider", () => {
-        const provider = new CryptKeeperInjectedProvider();
-        expect(provider.isCryptKeeper).toBe(true);
-    });
+  test("Should be able to check if it is CK provider", () => {
+    const provider = new CryptKeeperInjectedProvider();
+    expect(provider.isCryptKeeper).toBe(true);
+  });
 
-    test("Should be able to use the provider to make a connection", async () => {
-        const postMessageSpy = jest.spyOn(window, "postMessage");
-        //postMessageSpy.mockResolvedValue(["Result", null])
-        (window.postMessage as jest.Mock).mockResolvedValue(["Result", null]);
-        const provider = new CryptKeeperInjectedProvider();
-        (provider.connect as jest.Mock).mockResolvedValue(provider)
-        const client = await provider.connect();
-        expect(client).toBe(provider);
-    });
+  test("Should be able to use the provider to make a connection", async () => {
+    const postMessageSpy = jest.spyOn(window, "postMessage");
+    // postMessageSpy.mockResolvedValue(["Result", null])
+    (window.postMessage as jest.Mock).mockResolvedValue(["Result", null]);
+    const provider = new CryptKeeperInjectedProvider();
+    (provider.connect as jest.Mock).mockResolvedValue(provider);
+    const client = await provider.connect();
+    expect(client).toBe(provider);
+  });
 });
