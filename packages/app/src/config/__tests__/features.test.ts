be34587ec8684f8e2a2d4a90702841dafcf33ee5
0xmad <0xmad@users.noreply.github.com>
2023-07-06 15:14:16 -0500

    feat: enable reset password feature
    - [x] Remove feature flag for recover access and reset password

--- packages/app/src/config/__tests__/features.test.ts
+++ packages/app/src/config/__tests__/features.test.ts
@@ -6,26 +6,22 @@ describe("config/features", () => {
   const restoreEnv = {
     INTERREP_IDENTITY: process.env.INTERREP_IDENTITY,
     BACKUP: process.env.BACKUP,
-    RECOVER: process.env.RECOVER,
   };
 
   beforeAll(() => {
     process.env.INTERREP_IDENTITY = "true";
     process.env.BACKUP = "true";
-    process.env.RECOVER = "true";
   });
 
   afterAll(() => {
     process.env.INTERREP_IDENTITY = restoreEnv.INTERREP_IDENTITY;
     process.env.BACKUP = restoreEnv.BACKUP;
-    process.env.RECOVER = restoreEnv.RECOVER;
   });
 
   test("should return enabled features properly", () => {
     expect(getEnabledFeatures()).toStrictEqual({
       [Features.INTERREP_IDENTITY]: true,
       [Features.BACKUP]: true,
-      [Features.RECOVER]: true,
     });
   });
 });
