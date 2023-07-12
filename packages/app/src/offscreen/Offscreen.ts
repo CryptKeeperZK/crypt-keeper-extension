d821bf5c50af26e2a9f41925518998a2a99e9d8a
isk <iskander.s.andrews@gmail.com>
2023-07-12 16:39:00 +0200

    feat: working on offscreen controller

---
+++ packages/app/src/offscreen/Offscreen.ts
@@ -0,0 +1,12 @@
+
+export class Offscreen {
+    constructor() {
+
+    }
+
+
+    generateSemaphoreProof = () => {
+        
+    }
+}
+
--- packages/app/src/offscreen/index.ts
+++ packages/app/src/offscreen/index.ts
@@ -1,3 +1,25 @@
+<<<<<<< HEAD
 export function isTrueOffscreen(): boolean {
   return true;
+=======
+import log from "loglevel";
+import { RequestHandler } from "@cryptkeeperzk/types";
+import browser, { Runtime } from "webextension-polyfill";
+import { isDebugMode } from "@src/config/env";
+
+log.setDefaultLevel(isDebugMode() ? "debug" : "info");
+
+const offscreenMessageListener = async (request: RequestHandler, sender: Runtime.MessageSender) => {
+  log.debug("Inside Offscreen onMessage", request);
+
+  const {
+    method,
+    payload,
+    meta,
+    isOffscreen
+  } = request;
+  
+>>>>>>> 367f6fc (feat: working on offscreen controller)
 }
+
+browser.runtime.onMessage.addListener(offscreenMessageListener);
