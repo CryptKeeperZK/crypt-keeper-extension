103a68893652b8cc01ebb2fb8323ed48784821ad
Nick <nick@gmx.co.uk>
2023-07-18 10:09:53 +0300

    Manipulate .env in build script

--- .github/scripts/build.sh
+++ .github/scripts/build.sh
@@ -7,6 +7,8 @@ build=$1
 
 aws ecr get-login-password --region eu-central-1 | docker login --username AWS --password-stdin 490752553772.dkr.ecr.eu-central-1.amazonaws.com
 
+cp .env.example .evn
+
 docker build -f Dockerfile_demo -t crypt-keeper-extension-demo .
 docker tag crypt-keeper-extension-demo:latest 490752553772.dkr.ecr.eu-central-1.amazonaws.com/crypt-keeper-extension-demo:latest
 docker push 490752553772.dkr.ecr.eu-central-1.amazonaws.com/crypt-keeper-extension-demo:latest
