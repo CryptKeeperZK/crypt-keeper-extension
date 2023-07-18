7be5d50a34dfa8969813911692ad4428780c8c0d
Nick <nick@gmx.co.uk>
2023-07-18 09:30:25 +0300

    Fix role scripts + rename Dockerfiles

--- .github/scripts/build.sh
+++ .github/scripts/build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -ex
 
 build=$1
 
@@ -6,11 +7,11 @@ build=$1
 
 aws ecr get-login-password --region eu-central-1 | docker login --username AWS --password-stdin 490752553772.dkr.ecr.eu-central-1.amazonaws.com
 
-docker build -f apps/demo/Dockerfile -t crypt-keeper-extension-demo .
+docker build -f Dockerfile_demo -t crypt-keeper-extension-demo .
 docker tag crypt-keeper-extension-demo:latest 490752553772.dkr.ecr.eu-central-1.amazonaws.com/crypt-keeper-extension-demo:latest
 docker push 490752553772.dkr.ecr.eu-central-1.amazonaws.com/crypt-keeper-extension-demo:latest
 
-docker build -f apps/merkle/Dockerfile -t crypt-keeper-extension-merkle .
+docker build -f Dockerfile_merkle -t crypt-keeper-extension-merkle .
 docker tag crypt-keeper-extension-merkle:latest 490752553772.dkr.ecr.eu-central-1.amazonaws.com/crypt-keeper-extension-merkle:latest
 docker push 490752553772.dkr.ecr.eu-central-1.amazonaws.com/crypt-keeper-extension-merkle:latest
 
