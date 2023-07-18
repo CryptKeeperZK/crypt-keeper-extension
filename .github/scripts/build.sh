89c480deb84f8f33b2d84b06fd8e8fbd69bd7d91
Nick <nick@gmx.co.uk>
2023-07-18 18:36:44 +0300

    Move Dockerfiles into packages

--- .github/scripts/build.sh
+++ .github/scripts/build.sh
@@ -9,11 +9,11 @@ aws ecr get-login-password --region eu-central-1 | docker login --username AWS -
 
 cp .env.example .env
 
-docker build -f Dockerfile_demo -t crypt-keeper-extension-demo .
+docker build -f packages/demo/Dockerfile -t crypt-keeper-extension-demo .
 docker tag crypt-keeper-extension-demo:latest 490752553772.dkr.ecr.eu-central-1.amazonaws.com/crypt-keeper-extension-demo:latest
 docker push 490752553772.dkr.ecr.eu-central-1.amazonaws.com/crypt-keeper-extension-demo:latest
 
-docker build -f Dockerfile_merkle -t crypt-keeper-extension-merkle .
+docker build -f packages/merkle-mock-server/ -t crypt-keeper-extension-merkle .
 docker tag crypt-keeper-extension-merkle:latest 490752553772.dkr.ecr.eu-central-1.amazonaws.com/crypt-keeper-extension-merkle:latest
 docker push 490752553772.dkr.ecr.eu-central-1.amazonaws.com/crypt-keeper-extension-merkle:latest
 
