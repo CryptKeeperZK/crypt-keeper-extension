03bf432de1919b02ea1c48a7d574b017e8fb6d4c
0xmad <0xmad@users.noreply.github.com>
2023-09-12 17:37:47 -0500

    test: set max failures to zero

--- packages/e2e/playwright.config.ts
+++ packages/e2e/playwright.config.ts
@@ -14,6 +14,7 @@ export default defineConfig({
   expect: {
     timeout: 15_000,
   },
+  maxFailures: 0,
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 1 : 0,
