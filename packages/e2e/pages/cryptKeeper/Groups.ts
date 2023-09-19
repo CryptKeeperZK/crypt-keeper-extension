f505e1585c1cc7252381e40764588aab9fc58f65
Anton <14254374+0xmad@users.noreply.github.com>
2023-09-19 10:32:23 -0500

    test: group e2e tests (#881)
    Co-authored-by: 0xmad <0xmad@users.noreply.github.com>

---
+++ packages/e2e/pages/cryptKeeper/Groups.ts
@@ -0,0 +1,26 @@
+import type { Page } from "@playwright/test";
+
+import BasePage from "../BasePage";
+
+export default class Groups extends BasePage {
+  async joinGroupRequest(): Promise<Page> {
+    await this.page.getByText("Join test group").click({ delay: 1000 });
+    return this.page.context().waitForEvent("page");
+  }
+
+  async joinGroup(): Promise<void> {
+    const cryptKeeper = await this.joinGroupRequest();
+    await cryptKeeper.getByText("Accept", { exact: true }).click();
+  }
+
+  async generateGroupMerkleProofRequest(): Promise<Page> {
+    await this.page.getByText("Generate Group Merkle Proof").click({ delay: 1000 });
+    return this.page.context().waitForEvent("page");
+  }
+
+  async generateGroupMerkleProof(): Promise<void> {
+    const cryptKeeper = await this.generateGroupMerkleProofRequest();
+
+    await cryptKeeper.getByText("Accept", { exact: true }).click();
+  }
+}
