6b8cc94cac8d3c1f5936d4b82f85e2c3da0fe124
isk <0xisk>
2023-10-01 17:40:28 +0200

    fix: e2e

--- packages/e2e/tests/initialization.test.ts
+++ packages/e2e/tests/initialization.test.ts
@@ -28,9 +28,7 @@ test.describe("initialization", () => {
     await cryptKeeper.openBackupOnboarding();
     await cryptKeeper.createAccountFromBackup({ password: DEFAULT_BACKUP_PASSWORD, backupFilePath });
 
-    await page.getByText("Connect Identity", { exact: true }).click();
-    await cryptKeeper.connectIdentity();
-    await cryptKeeper.close();
+    await createAccount({ page, cryptKeeperExtensionId, context });
 
     await page.goto(`chrome-extension://${cryptKeeperExtensionId}/popup.html`);
     await expect(page.getByTestId("home-page")).toBeVisible();
