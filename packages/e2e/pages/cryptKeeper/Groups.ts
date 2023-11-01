8293abd1b19969e87b8a0f4d1dd703a7a4704b72
0xmad <0xmad@users.noreply.github.com>
2023-11-01 16:26:32 -0500

    chore: remove delays for e2e

--- packages/e2e/pages/cryptKeeper/Groups.ts
+++ packages/e2e/pages/cryptKeeper/Groups.ts
@@ -6,7 +6,7 @@ export default class Groups extends BasePage {
   async joinGroupRequest(): Promise<Page> {
     const [popup] = await Promise.all([
       this.page.context().waitForEvent("page"),
-      this.page.getByText("Join test group").click({ delay: 1000 }),
+      this.page.getByText("Join test group").click(),
     ]);
 
     return popup;
@@ -14,7 +14,7 @@ export default class Groups extends BasePage {
 
   async joinGroup(): Promise<Page> {
     const cryptKeeper = await this.joinGroupRequest();
-    await cryptKeeper.getByText("Accept", { exact: true }).click({ delay: 1000 });
+    await cryptKeeper.getByText("Accept", { exact: true }).click();
 
     return cryptKeeper;
   }
@@ -22,7 +22,7 @@ export default class Groups extends BasePage {
   async generateGroupMerkleProofRequest(): Promise<Page> {
     const [popup] = await Promise.all([
       this.page.context().waitForEvent("page"),
-      this.page.getByText("Generate Group Merkle Proof").click({ delay: 1000 }),
+      this.page.getByText("Generate Group Merkle Proof").click(),
     ]);
 
     return popup;
@@ -31,6 +31,6 @@ export default class Groups extends BasePage {
   async generateGroupMerkleProof(): Promise<void> {
     const cryptKeeper = await this.generateGroupMerkleProofRequest();
 
-    await cryptKeeper.getByText("Accept", { exact: true }).click({ delay: 1000 });
+    await cryptKeeper.getByText("Accept", { exact: true }).click();
   }
 }
